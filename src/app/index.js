63a82147476ebb4735e8d1ae3b92ca72c926bc07
alex18M <180873@upslp.edu.mx>
2023-01-14 16:39:19 -0600

    first commit

---
+++ src/app/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import App from './App';// Path: src\app\App.js
+
+render(<App/>, document.getElementById('app'));// Path: src\index.html
+
